6a382144fbe8f9d2553d44a32875706363d76088
Zach <z6chy@icloud.com>
2022-02-18 12:20:35 -0700

    Tables done

--- allSpice/dbSetup.sql
+++ allSpice/dbSetup.sql
@@ -6,3 +6,26 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+CREATE Table IF NOT EXISTS recipes(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  subtitle VARCHAR(255),
+  category VARCHAR(255) NOT NULL,
+  creatorId VARCHAR(255) NOT NULL,
+  FOREIGN KEY(creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+DROP Table recipes;
+CREATE Table IF NOT EXISTS ingrediants(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  quantity INT NOT NULL,
+  recipeId INT NOT NULL,
+  FOREIGN KEY(recipeId) REFERENCES recipes(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+CREATE Table IF NOT EXISTS steps(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  numberedSteps INT NOT NULL,
+  body VARCHAR(255) NOT NULL,
+  recipeId INT NOT NULL,
+  FOREIGN KEY(recipeId) REFERENCES recipes(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
